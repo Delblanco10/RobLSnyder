04fd7d1db7734df61094e39d17b4066ebf125e5b
Delblanco10 <trevorredyns@gmail.com>
2013-07-19 01:15:33 -0400

    Removed user command highlight feature.
    It caused style problems on my site, and there's a plugin that does the
    same thing anyway.

--- twentytwelve-child/comments.php
+++
@@ -1,6 +0,0 @@
-<li class=”<?php
-/* Only use the authcomment class from style.css if the user_id is 1 (admin) */
-if (1 == $comment->user_id)
-$oddcomment = “authcomment”;
-echo $oddcomment;
-?>” id=”comment…
