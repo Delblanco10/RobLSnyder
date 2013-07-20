c7e42d05fdfeda2cd0ae7e821d4177ccdfd6d5d1
Delblanco10 <trevorredyns@gmail.com>
2013-07-19 22:50:30 -0400

    Removed header image from all pages other than homepage.

--- twentytwelve-child/header.php
+++ twentytwelve-child/header.php
@@ -38,7 +38,7 @@
 
 
 		<?php $header_image = get_header_image();
-		if ( ! empty( $header_image ) ) : ?>
+		if ( ! empty( $header_image ) && is_home() ) : ?>
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
 		<?php endif; ?>
 		
